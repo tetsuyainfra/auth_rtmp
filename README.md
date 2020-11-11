262fc0c07abdc29e9b18e152726997a423b22b44
tetsuyainfra <tetsuyainfra@gmail.com>
2020-11-11 23:03:44 +0900

    runnable

---
+++ README.md
@@ -0,0 +1,13 @@
+# auth_rtmp
+
+nginx-rtmp-module have on_publish
+
+# example
+
+```
+cp auth_rtmp.yaml.example auth_rtmp.yaml
+vim auth_rtmp.yaml
+yarn start
+
+curl -vvv -X POST -d app=Foo name=Bar http://127.0.0.1:3010/
+```
--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-var express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const _ = require('lodash')
+const fs = require('fs')
+const yaml = require('js-yaml')
+
+const config = yaml.safeLoad(fs.readFileSync('auth_rtmp.yml', 'utf8'))
+const { host = "127.0.0.1", port = 3010 } = config
 
 const app = express()
-//const port = 3000
-const port = 4010
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-	console.log(req)
-	
-	//res.status(404).send()
-	res.status(200).send()
+app.post('/', (req, res) => {
+	if (_.find(config.authenticates, (auth_info) => {
+		return req.body.app === auth_info.app &&
+			req.body.name === auth_info.name
+	})) {
+		console.log(`access allow: `, req.body.app)
+		return res.status(200).send()
+	}
+
+	res.status(404).send()
 })
 
-app.listen(port,"127.0.0.1", () => {
-	  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port, host, () => {
+	console.log(`app listening at http://${host}:${port}`)
 })
-
-
 
